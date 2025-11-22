a24e2fe3eb08d38bac05b8ec04b5420b0b6c267f
senanv12 <sananhasanzadaforjob@gmail.com>
2025-11-22 17:11:56 +0400

    replace sort button with an image and refactor related functions for improved sorting functionality

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 let addTaskButton = document.querySelector('.add-task-btn');
-let listsContainer = document.querySelector('.lists');
+let lists = document.querySelector('.lists');
 let sortButton = document.querySelector('.sort');
 
-addTaskButton.addEventListener('click', () => {
-    addNewTask();
-});
+let ikinciclick = true;
 
+addTaskButton.addEventListener('click', () => {
+    addNew();
+});
 
 let initialSilButtons = document.querySelectorAll('.sil');
 initialSilButtons.forEach(button => {
-    button.addEventListener('click', removeTask);
+    button.addEventListener('click', remove);
 });
 
 let i = 2;
-function addNewTask() {
+function addNew() {
     let newList = document.createElement('div');
     newList.classList.add('list');
     newList.innerHTML = `
-        <p>${i}</p>
-        <input type="text" class="task-input" placeholder="">
-        <button class="sil">x</button>
+    <p>${i}</p>
+    <input type="text" class="task-input" placeholder="">
+    <button class="sil">x</button>
     `;
 
     const newSilButton = newList.querySelector('.sil');
-    newSilButton.addEventListener('click', removeTask);
-    listsContainer.appendChild(newList);
+    newSilButton.addEventListener('click', remove);
+    lists.appendChild(newList);
     i++;
     const newTaskInput = newList.querySelector('.task-input');
     newTaskInput.focus();
@@ -32,41 +33,71 @@ function addNewTask() {
 
 document.addEventListener('keydown', (e) => {
     if (e.key == 'Enter') {
-        addNewTask();
+        addNew();
     }
-
 })
 
-
-function removeTask(e) {
-    if (listsContainer.children.length > 1) {
+function remove(e) {
+    if (lists.children.length > 1) {
         const taskToRemove = e.target.parentNode;
-        listsContainer.removeChild(taskToRemove);
+        lists.removeChild(taskToRemove);
     }
-    else if (listsContainer.children.length === 1) {
-        listsContainer.children[0].querySelector('.task-input').value = '';
+    else if (lists.children.length === 1) {
+        lists.children[0].querySelector('.task-input').value = '';
     }
 }
 
+function sortlogo(isHovering) {
+    if (ikinciclick) {
+        if (isHovering) {
+
+            sortButton.src = "./images/sortdownblacks.svg";
+        } else {
+            sortButton.src = "./images/sortdowngrays.svg";
+        }
+    } else {
+        if (isHovering) {
+            sortButton.src = "./images/sortupblacks.svg";
+        } else {
+            sortButton.src = "./images/sortupgrays.svg";
+        }
+    }
+}
+
+sortButton.addEventListener('mouseenter', () => {
+    sortlogo(true);
+});
+
+sortButton.addEventListener('mouseleave', () => {
+    sortlogo(false);
+});
 
 sortButton.addEventListener('click', () => {
-    let tasks = Array.from(listsContainer.children);
-    tasks.sort((a, b) => {
-        const taskA = a.querySelector('.task-input').value.toLowerCase();
-        const taskB = b.querySelector('.task-input').value.toLowerCase();
+    let tasks = Array.from(lists.children);
 
-        if (taskA < taskB) {
-            return -1;
-        }
-        if (taskA > taskB) {
-            return 1;
-        }
-        return 0;
-    });
+    if (ikinciclick) {
+        tasks.sort((a, b) => {
+            const taskA = a.querySelector('.task-input').value.toLowerCase();
+            const taskB = b.querySelector('.task-input').value.toLowerCase();
 
-    listsContainer.innerHTML = '';
+            if (taskA < taskB) return -1;
+            if (taskA > taskB) return 1;
+            return 0;
+        });
+        ikinciclick = false;
+    } else {
+        tasks.sort((a, b) => {
+            const idA = parseInt(a.querySelector('p').textContent, 10);
+            const idB = parseInt(b.querySelector('p').textContent, 10);
+            return idA - idB;
+        });
+        ikinciclick = true;
+    }
 
+    lists.innerHTML = '';
     tasks.forEach(task => {
-        listsContainer.appendChild(task);
+        lists.appendChild(task);
     });
+
+    sortlogo(true);
 });
